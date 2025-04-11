5db869be56635270cf9d67a90f689f5f83ea643d
AndNath <andn23032@wainuiomatahigh.school.nz>
2025-04-11 14:29:43 +1200

    Update main.py
    expanded and added lists

--- main.py
+++ main.py
@@ -3,15 +3,34 @@ user_name = input("What is your name? ")
 print("Hello {}".format(user_name))
 score = 0
 
+# List the questions
+Question_list = [\
+'Q1: When did \"A Minecraft movie\" come out?\n A: 3rd of April\n B: 4th of April\n C: 1st of April\n',\
+'Q2: Who plays the character Steve?\n',\
+'Q3: What did \"Steve\" want as a kid?\n" A: To mine.\n B: To work.\n C: live wit his parents\n.']
+
+Answer_list = ['A','Jack Black','A']
+
 # Make the questions
-print("Q1: When did \"A Minecraft movie\" come out?") 3rd of April, 3/4, 3/4/25, April
+answer = input(Question_list[0])
+if answer == Answer_list[0]:
+    score += 10
+    print ("Good job\nYour new score is {}\n".format(score))
+else:
+    print ("Sorry, thats wrong\n your score will stay the same")
 
-print("Q2: Who plays the character Steve?") Jack Black, Black, Jack
+# "Q1: When did \"A Minecraft movie\" come out?\n A: 3rd of April\n B: 4th of April\n C: 1st of April/n
 
-print("Q3: What did \"Steve\" want as a kid?") To mine, He yearned for the mines
+# "Q2: Who plays the character Steve?\n") 
+# Jack black
 
-print("Q4: Finish the line \"Flint and _____\"?") Steel
+# "Q3: What did \"Steve\" want as a kid?\n" A: To mine.\n B: To work.\n C: live wit his parents\n.
 
-print("Q5: Finish the line \"_____ bucket release\"?") Water
+# "Q4: Finish the line \"Flint and _____\"\n"
+# Steel.
 
-print("Q6: What is the name of the movie \"A Minecraft movie\"?") A Minecraft movie, Bee movie
+# "Q5: Finish the line \"_____ bucket release\"\n" 
+# Water.
+
+# "Q6: What is the name of the movie \"A Minecraft movie\"?\n" 
+# A Minecraft movie
