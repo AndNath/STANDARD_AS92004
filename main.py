2a7391592d177c0253b22353ee2641647cdf803f
AndNath <andn23032@wainuiomatahigh.school.nz>
2025-04-22 12:07:45 +1200

    Update main.py
    Finished the program and learnt about len

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
 # Set the variables
-user_name = input("What is your name? ")
-print("Hello {}".format(user_name))
 score = 0
 
-# List the questions
+# List the questions & answer
 Question_list = [\
-'Q1: When did \"A Minecraft movie\" come out?\n A: 3rd of April\n B: 4th of April\n C: 1st of April\n',\
+'Q1: When did \"A Minecraft movie\" come out?\n A: 4rd of April\n B: 3th of April\n C: 1st of April\n',\
 'Q2: Who plays the character Steve?\n',\
-'Q3: What did \"Steve\" want as a kid?\n" A: To mine.\n B: To work.\n C: live wit his parents\n.']
+'Q3: What did \"Steve\" want as a kid?\n A: To mine.\n B: To work.\n C: live wit his parents.\n',\
+'Q4: Finish the line \"Flint and _____\"\n',\
+'Q5: Finish the line \"_____ bucket release\"\n',\
+'Q6: What is the name of the movie \"A Minecraft movie\"?\n']
 
-Answer_list = ['A','Jack Black','A']
+Answer_list = ['b','jack black','a','steel','water','a minecraft movie']
 
-# Make the questions
-answer = input(Question_list[0])
-if answer == Answer_list[0]:
-    score += 10
-    print ("Good job\nYour new score is {}\n".format(score))
-else:
-    print ("Sorry, thats wrong\n your score will stay the same")
+# Displaying the questions
+for i in range(len(Question_list)): # Repeats until it cant anymore
+    answer = input(Question_list[i])
+    if answer.lower() == Answer_list[i].lower():
+        score += 10
+        print("Good job\nYour new score is {}\n".format(score))
+    else:
+        print("Sorry, that's wrong\nYour score will stay at {}\n".format(score))
 
-# "Q1: When did \"A Minecraft movie\" come out?\n A: 3rd of April\n B: 4th of April\n C: 1st of April/n
-
-# "Q2: Who plays the character Steve?\n") 
-# Jack black
-
-# "Q3: What did \"Steve\" want as a kid?\n" A: To mine.\n B: To work.\n C: live wit his parents\n.
-
-# "Q4: Finish the line \"Flint and _____\"\n"
-# Steel.
-
-# "Q5: Finish the line \"_____ bucket release\"\n" 
-# Water.
-
-# "Q6: What is the name of the movie \"A Minecraft movie\"?\n" 
-# A Minecraft movie
+print("Your final score is {}/60".format(score))
+print("Thank you for playing my quiz about \"A Minecraft Movie\"\nI hope you enjoyed it")
