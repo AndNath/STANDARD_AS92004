eb791db01cf3025d585b651e9f0e4dabbf93cc55
AndNath <andn23032@wainuiomatahigh.school.nz>
2025-04-29 11:45:04 +1200

    Update main.py
    Added a question to the quiz.

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 # Set the variables
 score = 0
 
-# List the questions & answer
+# Lists of the questions & answers
 Question_list = [\
 'Q1: When did \"A Minecraft movie\" come out?\n A: 4rd of April\n B: 3th of April\n C: 1st of April\n',\
 'Q2: Who plays the character Steve?\n',\
-'Q3: What did \"Steve\" want as a kid?\n A: To mine.\n B: To work.\n C: live wit his parents.\n',\
+'Q3: What did \"Steve\" want as a kid?\n A: To mine.\n B: To work.\n C: live with his parents.\n',\
 'Q4: Finish the line \"Flint and _____\"\n',\
 'Q5: Finish the line \"_____ bucket release\"\n',\
-'Q6: What is the name of the movie \"A Minecraft movie\"?\n']
+'Q6: What is the name of the movie \"A Minecraft movie\"?\n',\
+'Q7: Where was \"A Minecraft Movie\" filmed?\n ']
 
-Answer_list = ['b','jack black','a','steel','water','a minecraft movie']
+Answer_list = ['b','jack black','a','steel','water','a minecraft movie','new zealand']
 
-# Displaying the questions
+# Displays the questions to the user
 for i in range(len(Question_list)): # Repeats until it cant anymore
     answer = input(Question_list[i])
     if answer.lower() == Answer_list[i].lower():
@@ -20,6 +21,6 @@ for i in range(len(Question_list)): # Repeats until it cant anymore
         print("Good job\nYour new score is {}\n".format(score))
     else:
         print("Sorry, that's wrong\nYour score will stay at {}\n".format(score))
-
-print("Your final score is {}/60".format(score))
+# Ending statement
+print("Your final score is {}/70".format(score))
 print("Thank you for playing my quiz about \"A Minecraft Movie\"\nI hope you enjoyed it")
