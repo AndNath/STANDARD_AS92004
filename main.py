3a7b7dca677c6ce5e67c32c6f2e08b509cf15d30
AndNath <andn23032@wainuiomatahigh.school.nz>
2025-04-29 12:56:56 +1200

    Update main.py
    Added time and intro

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-# Set the variables
+# Variables & Time
 score = 0
+import time
 
+# Intro to the quiz
+print("Welcome to the \"A Minecraft Movie\" quiz.\nI hope you have fun playing.\n")
+time.sleep(2)
 # Lists of the questions & answers
 Question_list = [\
-'Q1: When did \"A Minecraft movie\" come out?\n A: 4rd of April\n B: 3th of April\n C: 1st of April\n',\
-'Q2: Who plays the character Steve?\n',\
-'Q3: What did \"Steve\" want as a kid?\n A: To mine.\n B: To work.\n C: live with his parents.\n',\
-'Q4: Finish the line \"Flint and _____\"\n',\
-'Q5: Finish the line \"_____ bucket release\"\n',\
-'Q6: What is the name of the movie \"A Minecraft movie\"?\n',\
+'Q1: When did \"A Minecraft movie\" come out?\n A: 4rd of April\n B: 3th of April\n C: 1st of April\n ',\
+'Q2: Who plays the character Steve?\n ',\
+'Q3\n A: To mine.\n B: To work.\n C: live with his parents.\n ',\
+'Q4: Finish the line, \"Flint and _____\"\n ',\
+'Q5: Finish the line \"_____ bucket release\"\n ',\
+'Q6: What is the name of the movie \"A Minecraft movie\"?\n ',\
 'Q7: Where was \"A Minecraft Movie\" filmed?\n ']
 
 Answer_list = ['b','jack black','a','steel','water','a minecraft movie','new zealand']
@@ -16,11 +20,13 @@ Answer_list = ['b','jack black','a','steel','water','a minecraft movie','new zea
 # Displays the questions to the user
 for i in range(len(Question_list)): # Repeats until it cant anymore
     answer = input(Question_list[i])
+    time.sleep(1.5)
     if answer.lower() == Answer_list[i].lower():
         score += 10
-        print("Good job\nYour new score is {}\n".format(score))
+        print("\nGood job\nYour new score is {}\n".format(score))
     else:
-        print("Sorry, that's wrong\nYour score will stay at {}\n".format(score))
-# Ending statement
+        print("\nSorry, that's wrong\nYour score will stay at {}\n".format(score))
+a
+# Outro & final score
 print("Your final score is {}/70".format(score))
 print("Thank you for playing my quiz about \"A Minecraft Movie\"\nI hope you enjoyed it")
